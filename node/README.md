643622d6a83778c0f8fed8fdb4974a7f487ab7ac
Pwntus <pontus.edvard@gmail.com>
2019-06-21 19:49:54 +0200

    feat: Add Node.js template

---
+++ node/README.md
@@ -0,0 +1,2 @@
+# VisualBox Integration in Node.js
+This is a short and simple integration showing how a Node.js integration can be written.
---
+++ node/index.js
@@ -0,0 +1,39 @@
+/**
+ * The "visualbox" module is globally accessible
+ * and was copied in during the build phase.
+ */
+const visualbox = require('visualbox');
+
+/**
+ * The "figlet" module was installed since it was
+ * specified in the "package.json" file.
+ */
+const figlet = require('figlet');
+
+/**
+ * Read the configuration model values and log them.
+ */
+const { MODEL } = visualbox;
+console.log('My Variable: ', MODEL.myVariable);
+console.log('My Password: ', MODEL.myPassword);
+
+/**
+ * Start a simple loop which outputs a string
+ * to VisualBox.
+ */
+let n = 0;
+setInterval(() => {
+  n++;
+  figlet('Hello World! ' + n, (err, data) => {
+    if (err) {
+      console.log('Something went wrong...');
+      return;
+    }
+
+    /**
+     * The output() method of the "visualbox" module
+     * will send the data to a VisualBox dashboard.
+     */
+    visualbox.output(data);
+  });
+}, 5000);
