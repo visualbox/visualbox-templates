1daaf8c81c6d4206d765676a30fde5f9654c3df8
Pwntus <pontus.edvard@gmail.com>
2019-06-23 22:22:50 +0200

    feat: add Python 3.7 template

---
+++ integration-python/README.md
@@ -0,0 +1,2 @@
+# VisualBox Integration in Python 3.7
+This is a short and simple integration showing how a Python 3.7 integration can be written.
---
+++ integration-python/main.py
@@ -0,0 +1,27 @@
+"""
+The "visualbox" module is globally accessible
+and was copied in during the build phase.
+"""
+import visualbox
+"""
+The "pyfiglet" module was installed since it was
+specified in the "requirements.txt" file.
+"""
+from pyfiglet import Figlet
+from time import sleep
+
+# Read the configuration model values and log them.
+print("My Variable: %s" % visualbox.MODEL["myVariable"])
+print("My Password: %s" % visualbox.MODEL["myPassword"])
+
+"""
+Start a simple loop which outputs a string
+to VisualBox.
+"""
+n = 0
+f = Figlet(font='slant')
+
+while True:
+  n++
+  visualbox.output(f.renderText("Hello, World! %d" % n))
+  sleep(5)
